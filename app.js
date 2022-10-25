43e3d9e96cb3d044ece6e892b920437ed71ccf93
병아리 개발자 <101976498+Kamuie99@users.noreply.github.com>
2022-10-25 11:30:23 +0900

    간단한 계산기 만들기
    노마드 코더 강의

---
+++ app.js
@@ -0,0 +1,23 @@
+const calculator = {
+    plus: function (a, b){
+        console.log(a + b);
+    },
+    minus: function (a, b){
+        console.log(a - b);
+    },
+    times: function (a, b){
+        console.log(a * b);
+    },
+    divide: function (a, b){
+        console.log(a / b);
+    },
+    power: function (a, b){
+        console.log(a ** b);
+    },
+};
+
+calculator.plus(10, 5);
+calculator.minus(10, 5);
+calculator.times(10, 5);
+calculator.divide(10, 5);
+calculator.power(10, 5);

0fbbf2f9d93125edb4bd5656b9afa31b7a40d248
병아리 개발자 <101976498+Kamuie99@users.noreply.github.com>
2022-11-16 21:22:54 +0900

    DOM&Document_자바스크립트로 CSS3 스타일 동적 변경

---
+++ Ex08-02.js
@@ -0,0 +1,9 @@
+function change() {
+    var span = document.getElementById("mySpan");
+    span.style.color = "green";
+    span.style.fontSize = "30px";
+    span.style.display = "block";
+    span.style.width = "6em";
+    span.style.border = "3px dotted magenta";
+    span.style.margin = "20px";
+}

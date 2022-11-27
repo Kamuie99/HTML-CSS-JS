e094aa8c02ab0a44baa56857e9cfb9a501550792
병아리 개발자 <101976498+Kamuie99@users.noreply.github.com>
2022-11-27 16:50:29 +0900

    JavaScript_마우스 드래깅으로 캔버스에 그림 그리기

---
+++ Ex11-11.js
@@ -0,0 +1,39 @@
+var canvas, context;
+
+function init() {
+    canvas = document.getElementById("myCanvas");
+    context = canvas.getContext("2d");
+
+    context.lineWidth = 2;
+    context.strokeStyle = "blue";
+
+    canvas.addEventListener("mousemove", function (e) { moveBy(e) }, false);
+    canvas.addEventListener("mousedown", function (e) { down(e) }, false);
+    canvas.addEventListener("mouseup", function (e) { up(e) }, false);
+    canvas.addEventListener("mouseout", function (e) { out(e) }, false);
+}
+
+var startX = 0, startY = 0;
+var dragging = false;
+
+function draw(curX, curY) {
+    context.beginPath();
+    context.moveTo(startX, startY);
+    context.lineTo(curX, curY);
+    context.stroke();
+}
+
+function down(e) {
+    startX = e.offsetX; startY = e.offsetY; dragging = true;
+}
+
+function up(e) { dragging = false;}
+
+function move(e) {
+    if(!dragging) return;
+    var curX = e.offsetX, curY = e.offsetY;
+    draw(curX, curY);
+    startX = curX; startY = curY;
+}
+
+function out(e) { dragging = false;}
